7cc29afc86b294cdab3bd87b3c0475601bd40f99
DonVitalko <DonVitalko@mail.ru>
2021-08-08 08:16:13 +0300

    Homework Three

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -1,58 +1,48 @@
 package ru.gb.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-import java.util.Vector;
-
 import ru.gb.base.BaseScreen;
+import ru.gb.math.Rect;
+import ru.gb.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float SPEED = 0.02f;
+    private Texture bg;
+    private Background background;
 
-    private Texture background;
-    private Texture img;
     private Vector2 pos;
-    private Vector2 v;
-    private Vector2 touch;
 
     @Override
     public void show() {
         super.show();
-        background = new Texture("new.jpg");
-        img = new Texture("badlogic.jpg");
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
         pos = new Vector2();
-        v = new Vector2();
-        touch = new Vector2();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
-        if (touch.dst(pos) > SPEED){
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        img.dispose();
-        background.dispose();
+        bg.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight()-screenY);
-        v.set(touch.cpy().sub(pos)).scl(SPEED);
-        return false;
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
     }
 }
