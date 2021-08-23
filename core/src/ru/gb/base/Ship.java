e3beb8a9c31edb65638c34a20237cc43d5412147
DonVitalko <DonVitalko@mail.ru>
2021-08-23 04:03:12 +0300

    Homework seven

--- core/src/ru/gb/base/Ship.java
+++ core/src/ru/gb/base/Ship.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.math.Vector2;
 
 import ru.gb.math.Rect;
 import ru.gb.pool.BulletPool;
+import ru.gb.pool.ExplosionPool;
 import ru.gb.sprite.Bullet;
+import ru.gb.sprite.Explosion;
 
-public class Ship extends Sprite {
+public abstract class Ship extends Sprite {
 
-    private static final  float DAMAGE_ANIMATE_INTERVAL = 0.1f;
+    private static final float DAMAGE_ANIMATE_INTERVAL = 0.1f;
 
     protected final Vector2 v0;
     protected final Vector2 v;
 
     protected Rect worldBounds;
     protected BulletPool bulletPool;
+    protected ExplosionPool explosionPool;
     protected TextureRegion bulletRegion;
     protected Vector2 bulletPos;
     protected Vector2 bulletV;
@@ -54,15 +57,15 @@ public class Ship extends Sprite {
             reloadTimer = 0f;
             shoot();
         }
-        damageAnimateTimer +=delta;
-        if(damageAnimateTimer >= DAMAGE_ANIMATE_INTERVAL){
+        damageAnimateTimer += delta;
+        if (damageAnimateTimer >= DAMAGE_ANIMATE_INTERVAL) {
             frame = 0;
         }
     }
 
-    public void demage(int damage){
-        hp -=damage;
-        if (hp <= 0){
+    public void damage(int damage) {
+        hp -= damage;
+        if (hp <= 0) {
             hp = 0;
             destroy();
         }
@@ -70,9 +73,26 @@ public class Ship extends Sprite {
         damageAnimateTimer = 0f;
     }
 
+    public abstract boolean isBulletCollision(Bullet bullet);
+
+    public int getBulletDamage() {
+        return bulletDamage;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        boom();
+    }
+
     private void shoot() {
         Bullet bullet = bulletPool.obtain();
         bullet.set(this, bulletRegion, bulletPos, bulletV, bulletHeight, worldBounds, bulletDamage);
         bulletSound.play();
+    }
+
+    private void boom() {
+        Explosion explosion = explosionPool.obtain();
+        explosion.set(pos, getHeight());
     }
 }
